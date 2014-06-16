61c8af11a20dcf6c1161f925f6aa5462056065d0
pkourany <pkourany@gmail.com>
2014-06-15 20:36:35 -0400

    First commit

---
+++ st7735-test.ino
@@ -0,0 +1,32 @@
+
+#define cs   A2
+#define dc   D0
+#define rst  0  
+
+#include "Adafruit_mfGFX.h"    // Core graphics library
+#include "Adafruit_ST7735.h" // Hardware-specific library
+#include "fonts.h"
+
+Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst); // hardware spi
+
+void setup() {
+    
+     tft.initR(INITR_BLACKTAB);
+     
+     tft.fillScreen(ST7735_BLACK);
+
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST7735_WHITE);
+    tft.setTextWrap(true);
+    tft.print("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla");     
+    
+    tft.drawLine(0, 0, tft.width()-1, tft.height()-1, ST7735_YELLOW);
+    tft.drawLine(tft.width()-1, 0, 0, tft.height()-1, ST7735_YELLOW);
+
+    tft.drawPixel(0, tft.height()/2, ST7735_GREEN);
+
+}
+
+void loop() {
+
+}
